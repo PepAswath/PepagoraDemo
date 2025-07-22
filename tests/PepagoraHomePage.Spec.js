6d023c1ca8d966913be478d6470443ccfb513d59
PepAswath <pepagora.alphaqa@gmail.com>
2025-07-22 10:03:08 +0530

    Refresh PHPS

--- tests/PepagoraHomePage.Spec.js
+++ tests/PepagoraHomePage.Spec.js
@@ -1,22 +1,28 @@
 import { test, expect } from '@playwright/test';
 
+import { defineConfig } from '@playwright/test';
+
 test('test', async ({ page }) => {
 
-  await page.goto('https://staging.pepagora.com/',{ waitUntil: 'domcontentloaded' });
+  await page.goto('https://staging.pepagora.com/');
 
   await page.getByRole('button', { name: '×' }).click();
 
   await page.getByRole('banner').getByRole('link', { name: 'Post Buying Request' }).click();
 
+  await page.screenshot({ path: 'buying-request-filled.png' });
+
   await page.locator('#product_name').click();
 
   await page.locator('#product_name').fill('Tyre');
 
-  await page.getByRole('textbox', { name: 'Search Categories' }).waitFor({ state: 'visible', timeout: 60000 });
+  await page.getByRole('link', { name: 'Select Category' }).click();
 
   await page.getByRole('textbox', { name: 'Search Categories' }).click();
 
   await page.getByRole('textbox', { name: 'Search Categories' }).fill('Tyre');
+
+  await page.getByRole('button', { name: 'Search' }).click();
 
   await page.getByText('Rubber & Plastics >> Rubber').click();
 
@@ -30,7 +36,7 @@ test('test', async ({ page }) => {
 
   await page.getByRole('textbox', { name: 'Date' }).click();
 
-  await page.getByRole('link', { name: '25', exact: true }).click();
+  await page.getByRole('link', { name: '28', exact: true }).click();
 
   await page.locator('input[name="prd_first_name"]').click();
 
@@ -39,7 +45,9 @@ test('test', async ({ page }) => {
   await page.locator('input[name="prd_email_id"]').click();
 
   await page.locator('input[name="prd_email_id"]').fill('pepagoraalphaqa@gmail.com');
-  
+
   await page.getByRole('link', { name: 'Submit' }).click();
+
+  await page.screenshot({ path: 'form-submitted.png' });
 
 });
